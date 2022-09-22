c57ffbde83ae76d3343450da692f23ca147a8cff
AndrK189100 <andrka@gmail.com>
2022-09-22 20:29:16 +0300

    commit

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=256)
+    price = models.IntegerField()
+    image = models.URLField()
+    release_date = models.CharField(max_length=10)
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+
